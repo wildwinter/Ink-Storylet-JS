6d25a8bb0a6d9953e217876ad387de7e481ed0a5
wildwinter <ian@wildwinter.net>
2023-12-27 17:18:03 +0100

    Strip back HTML.

--- web/main.js
+++ web/main.js
@@ -1,327 +1,125 @@
 import { Storylets } from "../engine/storylets.js"
 
-(function (storyContent) {
-    //console.log("CREATE STORY");
-    // Create ink story from the content using inkjs
-    var story = new inkjs.Story(storyContent);
+var storyRoot = document.querySelector('#story');
 
-    // STORYLETS
-    var storylets = new Storylets(story);
-    storylets.onUpdated = function () {
-        continueStory(true);
-    };
+// Load Ink story.
+var story = new inkjs.Story(storyContent);
+
+// Set up Storylets
+var storylets = new Storylets(story);
+
+// Do this when the storylet availability check is completed
+storylets.onUpdated = onStoryletsUpdated;
+
+// Kick off storylet processing which will take at least a frame.
+updateStorylets();
+
+function updateStorylets() {
     storylets.StartUpdate();
-    // STORYLETS
+}
 
-    var savePoint = "";
+function scrollToBottom() {
+    storyRoot.scrollTop = storyRoot.scrollHeight;
+}
 
-    var storyContainer = document.querySelector('#story');
-    var outerScrollContainer = document.querySelector('.outerContainer');
+function onStoryletsUpdated() {
 
-    // page features setup
-    var hasSave = loadState();
-    setupButtons(hasSave);
+    if (storylets.GetAvailable().length == 0) {
+        var para = document.createElement('p');
+        para.innerHTML = "No storylets available. Story ended!"
+        storyRoot.appendChild(para);
 
-    // Set initial save point
-    savePoint = story.state.toJson();
+        scrollToBottom();
 
-    // Kick off the start of the story!
-    //continueStory(true);
+        return;
+    }
 
-    // Main story processing function. Each time this is called it generates
-    // all the next content up as far as the next set of choices.
-    function continueStory(firstTime) {
+    storylets.GetAvailable().forEach(function (storyletName) {
 
-        var paragraphIndex = 0;
-        var delay = 0.0;
+        // Create paragraph with anchor element
+        var para = document.createElement('p');
+        para.classList.add("storylet");
+        var content = `<a href='#'>${storyletName}</a>`;
+        var desc = storylets.getStoryletTag(storyletName, "desc", null);
+        if (desc != null)
+            content += `<div class="desc">${desc}</div>`;
 
-        // Don't over-scroll past new content
-        var previousBottomEdge = firstTime ? 0 : contentBottomEdgeY();
+        para.innerHTML = content;
 
-        // Generate story text - loop through available content
-        while (story.canContinue) {
+        storyRoot.appendChild(para);
 
-            // Get ink to generate the next paragraph
-            var paragraphText = story.Continue();
+        // Click on choice
+        var paraAnchor = para.querySelectorAll("a")[0];
+        paraAnchor.addEventListener("click", function (event) {
 
-            // Create paragraph element (initially hidden)
-            var paragraphElement = document.createElement('p');
-            paragraphElement.innerHTML = paragraphText;
-            storyContainer.appendChild(paragraphElement);
+            // Don't follow <a> link
+            event.preventDefault();
 
-            // Fade in paragraph after a short delay
-            showAfter(delay, paragraphElement);
-            delay += 200.0;
-        }
-
-        // Create HTML choices from ink choices
-        story.currentChoices.forEach(function (choice) {
-
-            // Create paragraph with anchor element
-            var choiceParagraphElement = document.createElement('p');
-            choiceParagraphElement.classList.add("choice");
-            choiceParagraphElement.innerHTML = `<a href='#'>${choice.text}</a>`
-            storyContainer.appendChild(choiceParagraphElement);
-
-            // Fade choice in after a short delay
-            showAfter(delay, choiceParagraphElement);
-            delay += 200.0;
-
-            // Click on choice
-            var choiceAnchorEl = choiceParagraphElement.querySelectorAll("a")[0];
-            choiceAnchorEl.addEventListener("click", function (event) {
-
-                // Don't follow <a> link
-                event.preventDefault();
-
-                // Remove all existing choices
-                removeAll(".choice");
-
-                // Tell the story where to go next
-                story.ChooseChoiceIndex(choice.index);
-
-                // This is where the save button will save from
-                savePoint = story.state.toJson();
-
-                // Aaand loop
-                continueStory();
-            });
+            // Tell the story where to go next
+            chooseStorylet(storyletName);
         });
+    });
 
-        // STORYLETS
-        if (story.currentChoices.length == 0) {
+    scrollToBottom();
+}
 
-            storylets.onUpdated = function () {
+function chooseStorylet(storyletName) {
+    storylets.ChooseStorylet(storyletName);
 
-                if (storylets.GetAvailable().length == 0) {
-                    var choiceParagraphElement = document.createElement('p');
-                    choiceParagraphElement.innerHTML = "No storylets available."
-                    storyContainer.appendChild(choiceParagraphElement);
-                    showAfter(delay, choiceParagraphElement);
-                    delay += 200.0;
-                }
+    runInk();
+}
 
-                storylets.GetAvailable().forEach(function (storyletName) {
+function runInk() {
 
-                    // Create paragraph with anchor element
-                    var choiceParagraphElement = document.createElement('p');
-                    choiceParagraphElement.classList.add("storylet");
-                    var content = `<a href='#'>${storyletName}</a>`;
-                    var desc = storylets.getStoryletTag(storyletName, "desc", null);
-                    if (desc != null)
-                        content += `<div class="desc">${desc}</div>`;
+    while (story.canContinue) {
 
-                    choiceParagraphElement.innerHTML = content;
+        // Get ink to generate the next paragraph
+        var paragraphText = story.Continue();
 
-                    storyContainer.appendChild(choiceParagraphElement);
-
-                    // Fade choice in after a short delay
-                    showAfter(delay, choiceParagraphElement);
-                    delay += 200.0;
-
-                    // Click on choice
-                    var choiceAnchorEl = choiceParagraphElement.querySelectorAll("a")[0];
-                    choiceAnchorEl.addEventListener("click", function (event) {
-
-                        // Don't follow <a> link
-                        event.preventDefault();
-
-                        // Remove all existing choices
-                        removeAll(".choice");
-                        removeAll(".storylet");
-
-                        // Tell the story where to go next
-                        storylets.ChooseStorylet(storyletName);
-
-                        // This is where the save button will save from
-                        savePoint = story.state.toJson();
-
-                        // Aaand loop
-                        continueStory();
-                    });
-                });
-            }
-            storylets.StartUpdate();
-        }
-        // END STORYLETS
-
-        // Extend height to fit
-        // We do this manually so that removing elements and creating new ones doesn't
-        // cause the height (and therefore scroll) to jump backwards temporarily.
-        storyContainer.style.height = contentBottomEdgeY() + "px";
-
-        if (!firstTime)
-            scrollDown(previousBottomEdge);
-
+        // Create paragraph element (initially hidden)
+        var para = document.createElement('p');
+        para.innerHTML = paragraphText;
+        storyRoot.appendChild(para);
     }
 
-    function restart() {
-        story.ResetState();
-        storylets.Reset();
-
-        setVisible(".header", true);
-
-        // set save point to here
-        savePoint = story.state.toJson();
-
-        continueStory(true);
-
-        outerScrollContainer.scrollTo(0, 0);
+    if (story.currentChoices.length == 0) {
+        updateStorylets();
+        return;
     }
 
-    // -----------------------------------
-    // Various Helper functions
-    // -----------------------------------
+    // Create HTML choices from ink choices
+    story.currentChoices.forEach(function (choice) {
 
-    // Fades in an element after a specified delay
-    function showAfter(delay, el) {
-        el.classList.add("hide");
-        setTimeout(function () { el.classList.remove("hide") }, delay);
-    }
+        // Create paragraph with anchor element
+        var para = document.createElement('p');
+        para.classList.add("choice");
+        para.innerHTML = `<a href='#'>${choice.text}</a>`
+        storyRoot.appendChild(para);
 
-    // Scrolls the page down, but no further than the bottom edge of what you could
-    // see previously, so it doesn't go too far.
-    function scrollDown(previousBottomEdge) {
+        // Click on choice
+        var paraAnchor = para.querySelectorAll("a")[0];
+        paraAnchor.addEventListener("click", function (event) {
 
-        // Line up top of screen with the bottom of where the previous content ended
-        var target = previousBottomEdge;
+            // Don't follow <a> link
+            event.preventDefault();
 
-        // Can't go further than the very bottom of the page
-        var limit = outerScrollContainer.scrollHeight - outerScrollContainer.clientHeight;
-        if (target > limit) target = limit;
-
-        var start = outerScrollContainer.scrollTop;
-
-        var dist = target - start;
-        var duration = 300 + 300 * dist / 100;
-        var startTime = null;
-        function step(time) {
-            if (startTime == null) startTime = time;
-            var t = (time - startTime) / duration;
-            var lerp = 3 * t * t - 2 * t * t * t; // ease in/out
-            outerScrollContainer.scrollTo(0, (1.0 - lerp) * start + lerp * target);
-            if (t < 1) requestAnimationFrame(step);
-        }
-        requestAnimationFrame(step);
-    }
-
-    // The Y coordinate of the bottom end of all the story content, used
-    // for growing the container, and deciding how far to scroll.
-    function contentBottomEdgeY() {
-        var bottomElement = storyContainer.lastElementChild;
-        return bottomElement ? bottomElement.offsetTop + bottomElement.offsetHeight : 0;
-    }
-
-    // Remove all elements that match the given selector. Used for removing choices after
-    // you've picked one, as well as for the CLEAR and RESTART tags.
-    function removeAll(selector) {
-        var allElements = storyContainer.querySelectorAll(selector);
-        for (var i = 0; i < allElements.length; i++) {
-            var el = allElements[i];
-            el.parentNode.removeChild(el);
-        }
-    }
-
-    // Used for hiding and showing the header when you CLEAR or RESTART the story respectively.
-    function setVisible(selector, visible) {
-        var allElements = storyContainer.querySelectorAll(selector);
-        for (var i = 0; i < allElements.length; i++) {
-            var el = allElements[i];
-            if (!visible)
-                el.classList.add("invisible");
-            else
-                el.classList.remove("invisible");
-        }
-    }
-
-    // Helper for parsing out tags of the form:
-    //  # PROPERTY: value
-    // e.g. IMAGE: source path
-    function splitPropertyTag(tag) {
-        var propertySplitIdx = tag.indexOf(":");
-        if (propertySplitIdx != null) {
-            var property = tag.substr(0, propertySplitIdx).trim();
-            var val = tag.substr(propertySplitIdx + 1).trim();
-            return {
-                property: property,
-                val: val
-            };
-        }
-
-        return null;
-    }
-
-    // Loads save state if exists in the browser memory
-    function loadState() {
-
-        try {
-            var loaded = true;
-
-            let savedState = window.localStorage.getItem('save-state');
-            if (savedState) {
-                story.state.LoadJson(savedState);
-            }
-            else {
-                loaded = false;
-            }
-
-            if (loaded) {
-                var savedStorylet = window.localStorage.getItem('save-storylets');
-                if (savedStorylet) {
-                    storylets.LoadJson(savedStorylet);
-                }
-                else {
-                    loaded = false;
-                }
-            }
-
-            return loaded;
-        } catch (e) {
-            console.debug("Couldn't load save state");
-        }
-        return false;
-    }
-
-    function saveState() {
-        try {
-            window.localStorage.setItem('save-state', savePoint);
-            window.localStorage.setItem('save-storylets', storylets.SaveJson());
-        } catch (e) {
-            console.warn("Couldn't save state");
-        }
-
-    }
-
-    // Used to hook up the functionality for global functionality buttons
-    function setupButtons(hasSave) {
-
-        let rewindEl = document.getElementById("rewind");
-        if (rewindEl) rewindEl.addEventListener("click", function (event) {
-            removeAll("p");
-            removeAll("img");
-            setVisible(".header", false);
-            restart();
+            chooseChoice(choice.index);
         });
+    });
 
-        let saveEl = document.getElementById("save");
-        if (saveEl) saveEl.addEventListener("click", function (event) {
-            saveState();
-            document.getElementById("reload").removeAttribute("disabled");
-        });
+    scrollToBottom();
+}
 
-        let reloadEl = document.getElementById("reload");
-        if (!hasSave) {
-            reloadEl.setAttribute("disabled", "disabled");
-        }
-        reloadEl.addEventListener("click", function (event) {
-            if (reloadEl.getAttribute("disabled"))
-                return;
+function chooseChoice(index) {
+    story.ChooseChoiceIndex(index);
+    runInk();
+}
 
-            removeAll("p");
-            removeAll("img");
-            loadState();
-            continueStory(true);
-        });
+/*
+function removeAll(selector) {
+    var allElements = storyContainer.querySelectorAll(selector);
+    for (var i = 0; i < allElements.length; i++) {
+        var el = allElements[i];
+        el.parentNode.removeChild(el);
     }
-
-})(storyContent);
+}*/
